9f89fb2aa12432b5e125cfbea6c1e61fb5e42b2f
NguyenLarissa <larissanguyen83@gmail.com>
2024-12-16 23:09:58 +0100

    ajout du Calendrier

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,22 @@
 // import { inject } from 'aurelia-framework';
 
 // @inject()
+
+import { Calendar } from '../calendar/calendar';
+import { resolve } from 'aurelia';
+import { IRouter, RouteableComponent } from '@aurelia/router-lite';
+
+
 export class App {
+  private readonly router: IRouter = resolve(IRouter);
   constructor() {}
 
   // Méthode pour gérer les clics sur les boutons
   onButtonClick(buttonName: string) {
-    alert(`Button clicked: ${buttonName}`);
+    if (buttonName === 'Schedule') {
+      this.router.load('schedule'); // Redirige vers la route 'schedule'
+    } else {
+      alert(`Button clicked: ${buttonName}`);
+    }
   }
 }
